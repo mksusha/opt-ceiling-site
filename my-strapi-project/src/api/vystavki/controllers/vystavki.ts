746da478e5238addfd65307b8ae3829fd2d1d049
Ksusha <tsp.odett@gmail.com>
2024-12-18 12:32:44 +0300

    Removed Strapi backend from the project

--- my-strapi-project/src/api/vystavki/controllers/vystavki.ts
+++
@@ -1,7 +0,0 @@
-/**
- * vystavki controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::vystavki.vystavki');
--- my-strapi-project/src/api/vystavki/services/vystavki.ts
+++
@@ -1,7 +0,0 @@
-/**
- * vystavki service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::vystavki.vystavki');
